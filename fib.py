4f1cbaaca169454a42cc81aa5b3dee8a0a274ad6
eiphyuphyumoe <eiphyuphyu112@gmail.com>
2019-11-12 22:58:31 +0630

    Fibonacci

---
+++ fib.py
@@ -0,0 +1,24 @@
+import Fibo
+
+# Fibo.fib(500)
+
+# f = Fibo.fib
+# f(100)
+
+# Fibo.fib(10000)
+
+from Fibo import fib, fib2
+
+f = Fibo.fib2(500)
+print(f)
+
+
+
+# from Fibo import *
+# fib(500)
+
+# import Fibo as fi
+# fi.fib(500)
+
+# from Fibo import fib as fibonacci
+# fibonacci(500)
